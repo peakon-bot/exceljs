5bacbfa28a782b423596028985fad47c63999363
Andreas Lind <andreas.lind@peakon.com>
2019-05-12 21:46:23 +0200

    Leave a noop error handler on the sax stream when aborting due to an error
    Prevents a parse error from causing an unhandled rejection
    
    (cherry picked from commit 26a70191d79ac3111766098b3782a7d82e91935b)

--- spec/integration/workbook-xlsx-reader.spec.js
+++ spec/integration/workbook-xlsx-reader.spec.js
@@ -172,4 +172,33 @@ describe('WorkbookReader', function() {
       expect(cell.value).to.equal('_x000D_');
     });
   });
+
+  describe('with a spreadsheet that has an XML parse error in a worksheet', function () {
+    it('should reject the promise with the sax error', function () {
+      let unhandledRejection;
+      function unhandledRejectionHandler(err) {
+        unhandledRejection = err;
+      }
+      process.on('unhandledRejection', unhandledRejectionHandler);
+      const workbook = new Excel.Workbook();
+      return workbook.xlsx
+        .readFile('./spec/integration/data/invalid-xml.xlsx')
+        .then(
+          () => {
+            throw new Error('Promise unexpectedly fulfilled');
+          },
+          err => {
+            expect(err.message).to.equal('Text data outside of root node.\nLine: 1\nColumn: 1\nChar: f');
+            // Wait a tick before checking for an unhandled rejection
+            return new Promise(setImmediate);
+          }
+        )
+        .then(() => {
+          expect(unhandledRejection).to.be.undefined;
+        })
+        .finally(() => {
+          process.removeListener('unhandledRejection', unhandledRejectionHandler);
+        });
+    });
+  });
 });
